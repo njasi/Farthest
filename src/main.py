12ea9fdc97dc014dd0b9568b094f4ac14d01abf2
njasi <nick@jasinski3.com>
2024-04-27 00:16:12 -0700

    keep track of the outdated pip packages, add patch for message timeout

--- src/main.py
+++ src/main.py
@@ -10,7 +10,11 @@ import audioread
 import requests
 import traceback
 
-from datetime import datetime
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+from datetime import datetime, timedelta
 from yt_dlp import YoutubeDL
 
 
@@ -19,11 +23,10 @@ from telegram import Update
 from telegram.ext import Filters, Updater
 from telegram.ext import CallbackContext, CommandHandler, MessageHandler
 import os
+
 src = os.path.dirname(os.path.realpath(__file__))
 
-ydl_opts = {
-    'quiet': True
-}
+ydl_opts = {"quiet": True}
 youtube_dl_manager = yt_dlp.YoutubeDL(ydl_opts)
 
 
@@ -35,16 +38,18 @@ BOT_USERNAME = config["username"]
 MAX_VOLUME = 100
 
 # check if url is youtube vid
-YOUTUBE_REGEX = '^(https?\:\/\/)?((www\.)?youtube\.com|youtu\.be)\/.+$'
-RAW_REGEX = '^.*(?=\.wav$|\.mp3$)'
-TYPE_REGEX = '\.mp3'
-URL_REGEX = '^((http|https)://)[-a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)$'
+YOUTUBE_REGEX = "^(https?\:\/\/)?((www\.)?youtube\.com|youtu\.be)\/.+$"
+RAW_REGEX = "^.*(?=\.wav$|\.mp3$)"
+TYPE_REGEX = "\.mp3"
+URL_REGEX = "^((http|https)://)[-a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)$"
+STALE_TIMEOUT = 120
 
 # config bot
 updater = Updater(config["token"], use_context=True)
 dispatcher = updater.dispatcher
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 
 # config audio queue
 QUEUE = AudioQueue(updater.bot)
@@ -58,7 +63,7 @@ def test_length(url):
     ext = ext.group(0)
     name = f"temp{ext}"
     try:
-        with open(name, 'wb') as f:
+        with open(name, "wb") as f:
             doc = requests.get(url)
             f.write(doc.content)
         with audioread.audio_open(name) as f:
@@ -85,8 +90,8 @@ def is_quiet_hour():
     return start <= now and now <= end
 
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 # garbage socket listener for quiet hour start
 
@@ -114,36 +119,52 @@ def quiet_wrapper():
 
 
 def usage_dispatcher(message, update):
-    return lambda: updater.bot.send_message(chat_id=update.effective_chat.id, text=message, parse_mode="HTML")
+    return lambda: updater.bot.send_message(
+        chat_id=update.effective_chat.id, text=message, parse_mode="HTML"
+    )
 
 
 def start(update: Update, context: CallbackContext):
     # start command
     context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="I'm a bot, please talk to me!")
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+    )
 
 
 def handler_handler(handler):
     # garbage filter to avoid crashing
     def callback(update: Update, context: CallbackContext):
         try:
-            admin = str(update.message.from_user.id) in config["admins"]
-            if not admin and not str(update.effective_chat.id) == config["farther_chat_id"]:
-                context.bot.send_message(chat_id=update.effective_chat.id,
-                                         text="You can only use this bot in the Farthest chat.")
+            if (
+                datetime.now().timestamp() - update.message.date.timestamp()
+                > STALE_TIMEOUT
+            ):
                 return
+
+            admin = str(update.message.from_user.id) in config["admins"]
+            if (
+                not admin
+                and not str(update.effective_chat.id) == config["farther_chat_id"]
+            ):
+                context.bot.send_message(
+                    chat_id=update.effective_chat.id,
+                    text="You can only use this bot in the Farthest chat.",
+                )
+                return
+
             handler(update, context)
         except Exception as e:
             traceback.print_exc()
             for admin in config["admins"]:
                 updater.bot.send_message(chat_id=admin, text=e.__str__())
+
     return callback
 
 
 def add_callback(update: Update, context: CallbackContext):
     usage = usage_dispatcher(
-        "<b>Usage:</b>\n/add song\n\tsong: a youtube video link, or search term", update)
+        "<b>Usage:</b>\n/add song\n\tsong: a youtube video link, or search term", update
+    )
 
     # check if url is youtube link, if so add to queue
     if len(context.args) < 1:
@@ -171,7 +192,8 @@ def raw_add(phrase, update=None):
         phrase,
         update.message.from_user.full_name,
         test_length(phrase),
-        update.message.from_user.id)
+        update.message.from_user.id,
+    )
     audio.type = "RAW"
     QUEUE.add_audio(audio)
 
@@ -186,40 +208,41 @@ def youtube_search_add(term, url=False, update=None):
         result["title"],
         update.message.from_user.full_name,
         result["duration"],
-        update.message.from_user.id)
+        update.message.from_user.id,
+    )
     QUEUE.add_audio(audio)
 
     return result
 
 
 def youtube_search(term, amount=1, url=False, update=None):
-    '''
+    """
     use yt_dlp to find the first non livestream youtube
     video, and return the important attributes
 
     if you want to print for debug you have to do
     this to get actual json:
         print(json.dumps(ydl.sanitize_info(info)))
-    '''
+    """
     ydl_opts = {
-        'match_filter': yt_dlp.utils.match_filter_func("!is_live"),
-        'noplaylist': "True" if url else "False",
-        'format': 'worstaudio/worst'
+        "match_filter": yt_dlp.utils.match_filter_func("!is_live"),
+        "noplaylist": "True" if url else "False",
+        "format": "worstaudio/worst",
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = {}
         out_vid = {"placeholder": True}
         if url:
-            info = ydl.extract_info(
-                url=term,
-                download=False
-            )
+            info = ydl.extract_info(url=term, download=False)
             if "_type" in info and info["_type"] == "playlist":
                 results = []
                 for entry in info["entries"]:
-                    results += [youtube_search_add(
-                        entry["webpage_url"], url=True, update=update)]
+                    results += [
+                        youtube_search_add(
+                            entry["webpage_url"], url=True, update=update
+                        )
+                    ]
                 return {"result": "ok", "type": "playlist", "list": results}
             else:
                 out_vid = info
@@ -234,7 +257,7 @@ def youtube_search(term, amount=1, url=False, update=None):
             if amount > 20:
                 return {
                     "result": "err",
-                    "description": "All videos parsed for this search term were livestreams."
+                    "description": "All videos parsed for this search term were livestreams.",
                 }
             elif "placeholder" in out_vid:
                 return youtube_search(term, amount=amount + 5, update=update)
@@ -256,48 +279,56 @@ def spotify():
 
 
 def volume_callback(update: Update, context: CallbackContext):
-    if is_quiet_hour():
-        if not str(update.message.from_user.id) in config["admins"]:
-            context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text=f"You cannot adjust the volume during quiet hours.",
-                                     parse_mode="HTML",
-                                     reply_to_message_id=update.message.message_id)
-            return
+    # just commented out so u can change the volume at all times
+    # if is_quiet_hour():
+    #     if not str(update.message.from_user.id) in config["admins"]:
+    #         context.bot.send_message(chat_id=update.effective_chat.id,
+    #                                  text=f"You cannot adjust the volume during quiet hours.",
+    #                                  parse_mode="HTML",
+    #                                  reply_to_message_id=update.message.message_id)
+    #         return
 
     usage = usage_dispatcher(
-        "<b>Usage:</b>\n/volume [level [-d | -u]]\n\tVolume must be an integer from 0-100\n\t-d makes the volume go down by level\n\t -u makes the volume go up by level", update)
+        "<b>Usage:</b>\n/volume [level [-d | -u]]\n\tVolume must be an integer from 0-100\n\t-d makes the volume go down by level\n\t -u makes the volume go up by level",
+        update,
+    )
 
     if len(context.args) == 0:
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text=f"The current volume is {QUEUE.volume}%",
-                                 parse_mode="HTML",
-                                 reply_to_message_id=update.message.message_id)
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"The current volume is {QUEUE.volume}%",
+            parse_mode="HTML",
+            reply_to_message_id=update.message.message_id,
+        )
         return
     if len(context.args) != 1:
         usage()
     value = int(context.args[0])
     if value <= MAX_VOLUME:
         QUEUE.set_volume(value)
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text=f"Volume set to {value}%",
-                                 parse_mode="HTML",
-                                 reply_to_message_id=update.message.message_id)
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"Volume set to {value}%",
+            parse_mode="HTML",
+            reply_to_message_id=update.message.message_id,
+        )
     else:
         usage()
 
 
 def mute_callback(update: Update, context: CallbackContext):
-    usage = usage_dispatcher(
-        "<b>Usage:</b>\n/mute", update)
+    usage = usage_dispatcher("<b>Usage:</b>\n/mute", update)
 
     if len(context.args) != 0:
         usage()
         return
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text=f"Muted farther. use /volume to give farther a voice again",
-                             parse_mode="HTML",
-                             disable_web_page_preview=True,
-                             reply_to_message_id=update.message.message_id)
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"Muted farther. use /volume to give farther a voice again",
+        parse_mode="HTML",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message.message_id,
+    )
     QUEUE.set_volume(0)
 
 
@@ -306,30 +337,38 @@ def pause_callback(update: Update, context: CallbackContext):
 
     current = QUEUE.currently_playing
     if not current:
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text=f"There is nothing currently queued, use /add to add things to the queue.",
-                                 reply_to_message_id=update.message.message_id)
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"There is nothing currently queued, use /add to add things to the queue.",
+            reply_to_message_id=update.message.message_id,
+        )
         return
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text=f"Paused <a href='{QUEUE.currently_playing.url}'>{QUEUE.currently_playing.title}</a>. \n/play to resume.",
-                             parse_mode="HTML",
-                             disable_web_page_preview=True,
-                             reply_to_message_id=update.message.message_id)
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"Paused <a href='{QUEUE.currently_playing.url}'>{QUEUE.currently_playing.title}</a>. \n/play to resume.",
+        parse_mode="HTML",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message.message_id,
+    )
 
 
 def play_callback(update: Update, context: CallbackContext):
     QUEUE.play()
     current = QUEUE.currently_playing
     if not current:
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text=f"There is nothing currently queued, use /add to add things to the queue.",
-                                 reply_to_message_id=update.message.message_id)
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"There is nothing currently queued, use /add to add things to the queue.",
+            reply_to_message_id=update.message.message_id,
+        )
         return
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text=f"Playing <a href='{current.url}'>{current.title}</a>.\n/pause to pause.",
-                             disable_web_page_preview=True,
-                             parse_mode="HTML",
-                             reply_to_message_id=update.message.message_id)
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"Playing <a href='{current.url}'>{current.title}</a>.\n/pause to pause.",
+        disable_web_page_preview=True,
+        parse_mode="HTML",
+        reply_to_message_id=update.message.message_id,
+    )
 
 
 def skip_callback(update: Update, context: CallbackContext):
@@ -343,12 +382,18 @@ def skip_callback(update: Update, context: CallbackContext):
             except:
                 pass
     skipped = QUEUE.skip()
-    text = f"Skipped <a href='{skipped.url}'>{skipped.title}</a>." if skipped != None else "The queue is empty, there is nothing to skip. Add things to the queue with /add"
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text=text,
-                             disable_web_page_preview=True,
-                             parse_mode="HTML",
-                             reply_to_message_id=update.message.message_id)
+    text = (
+        f"Skipped <a href='{skipped.url}'>{skipped.title}</a>."
+        if skipped != None
+        else "The queue is empty, there is nothing to skip. Add things to the queue with /add"
+    )
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text,
+        disable_web_page_preview=True,
+        parse_mode="HTML",
+        reply_to_message_id=update.message.message_id,
+    )
 
 
 def queue_callback(update: Update, context: CallbackContext):
@@ -360,44 +405,46 @@ def queue_callback(update: Update, context: CallbackContext):
         text=QUEUE.queue_to_string(),
         parse_mode="HTML",
         disable_web_page_preview=True,
-        reply_to_message_id=update.message.message_id)
+        reply_to_message_id=update.message.message_id,
+    )
 
 
 def init_handlers():
     # setup all handlers
-    dispatcher.add_handler(CommandHandler(
-        ['add', 'a'], handler_handler(add_callback)))
-    dispatcher.add_handler(CommandHandler(
-        'pause', handler_handler(pause_callback)))
-    dispatcher.add_handler(CommandHandler(
-        'play', handler_handler(play_callback)))
-    dispatcher.add_handler(CommandHandler(
-        'skip', handler_handler(skip_callback)))
-    dispatcher.add_handler(CommandHandler(
-        ['queue', 'q'], handler_handler(queue_callback)))
-    dispatcher.add_handler(CommandHandler(
-        ['volume', 'v'], handler_handler(volume_callback)))
-    dispatcher.add_handler(CommandHandler(
-        'mute', handler_handler(mute_callback)))
+    dispatcher.add_handler(CommandHandler(["add", "a"], handler_handler(add_callback)))
+    dispatcher.add_handler(CommandHandler("pause", handler_handler(pause_callback)))
+    dispatcher.add_handler(CommandHandler("play", handler_handler(play_callback)))
+    dispatcher.add_handler(CommandHandler("skip", handler_handler(skip_callback)))
+    dispatcher.add_handler(
+        CommandHandler(["queue", "q"], handler_handler(queue_callback))
+    )
+    dispatcher.add_handler(
+        CommandHandler(["volume", "v"], handler_handler(volume_callback))
+    )
+    dispatcher.add_handler(CommandHandler("mute", handler_handler(mute_callback)))
 
 
 def connect():
     # cursed connection check
     try:
-        urllib.request.urlopen('https://www.google.com')
+        urllib.request.urlopen("https://www.google.com")
         return True
-    except:
+    except Exception as e:
         return False
 
 
 if __name__ == "__main__":
     init_handlers()
-    quiet_thread = threading.Thread(target=quiet_wrapper,)
+    quiet_thread = threading.Thread(
+        target=quiet_wrapper,
+    )
     quiet_thread.start()
     print("Checking network connection...")
     while True:
         if connect():
             print("\tConnected")
+            if is_quiet_hour():
+                QUEUE.set_volume(0)
             updater.start_polling()
             break
         else:
